bd4b4a760306c50c1c5e1fad3ced9c778594da88
HP@LAPTOP <baxtibaybekniyazov@gmail.com>
2024-02-06 22:58:50 +0500

    Add timer function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+window.addEventListener('DOMContentLoaded', () => {
   // LOADER start
   const elLoaderWrapper = document.querySelector('.loader-wrapper')
 
@@ -5,3 +6,54 @@
     elLoaderWrapper.style.display = 'none'
   }, 1000)
   // LOADER end
+
+  // TIMER start
+  const deadline = '2024-03-01'
+  function getTimeRemaining(endtime) {
+    const time = Date.parse(endtime) - Date.parse(new Date()),
+      days = Math.floor(time / (1000 * 60 * 60 * 24)),
+      hours = Math.floor((time / (1000 * 60 * 60)) % 24),
+      minutes = Math.floor((time / (1000 * 60)) % 60),
+      seconds = Math.floor((time / (1000)) % 60)
+
+    return {
+      totalTime: time,
+      days,
+      hours,
+      minutes,
+      seconds
+    }
+  }
+
+  function formatNumber(number) {
+    if(number >= 0 && number < 10) {
+      return `0${number}`
+    } else {
+      return number
+    }
+  }
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+      days = timer.querySelector('#days')
+      hours = timer.querySelector('#hours')
+      minutes = timer.querySelector('#minutes')
+      seconds = timer.querySelector('#seconds')
+      timeInerval = setInterval(updateClock, 1000)
+
+    function updateClock() {
+      const time = getTimeRemaining(endtime)
+      days.textContent = formatNumber(time.days)
+      hours.textContent = formatNumber(time.hours)
+      minutes.textContent = formatNumber(time.minutes)
+      seconds.textContent = formatNumber(time.seconds)
+
+      if(time.totalTime <= 0) {
+        clearInterval(timeInerval)
+      }
+    }
+  }
+
+  setClock('.timer', deadline)
+  // TIMER end
+})
