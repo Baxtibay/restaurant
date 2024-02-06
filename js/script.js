21903744608d1ba5fcc469e89b1d196768a16013
HP@LAPTOP <baxtibaybekniyazov@gmail.com>
2024-02-06 22:57:17 +0500

    Add loader

---
+++ js/script.js
@@ -0,0 +1,7 @@
+  // LOADER start
+  const elLoaderWrapper = document.querySelector('.loader-wrapper')
+
+  setTimeout(() => {
+    elLoaderWrapper.style.display = 'none'
+  }, 1000)
+  // LOADER end
