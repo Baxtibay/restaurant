97d95423c8e3b035b57059321357e0f44829e7cb
HP@LAPTOP <baxtibaybekniyazov@gmail.com>
2024-02-09 22:07:14 +0500

    Add tabcontents show function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,45 @@
 window.addEventListener('DOMContentLoaded', () => {
+  // SHOW TAB CONTENT FUNCTION
+  const elTabs = document.querySelectorAll('.tabheader__item'),
+    elsTabContents = document.querySelectorAll('.tab_content'),
+    elTabParent = document.querySelector('.tabheader__items')
+
+    function hideTabContents() {
+      elsTabContents.forEach(tabContent => {
+        tabContent.classList.add('hide')
+        tabContent.classList.remove('show')
+      })
+
+      elTabs.forEach(tab => {
+        tab.classList.remove('tabheader__item_active')
+      })
+    }
+
+    function showTabContent(index = 0) {
+      elsTabContents[index].classList.add('show', 'fade')
+      elsTabContents[index].classList.remove('hide')
+      elTabs[index].classList.add('tabheader__item_active')
+    }
+
+    hideTabContents()
+    showTabContent()
+
+    if(elTabParent) {
+      elTabParent.addEventListener('click', (evt) => {
+        const target = evt.target
+
+        if(target && target.classList.contains('tabheader__item')) {
+          elTabs.forEach((tab, index) => {
+            if(target === tab) {
+              hideTabContents()
+              showTabContent(index)
+            }
+          })
+        }
+      })
+    }
+
+
   // LOADER start
   const elLoaderWrapper = document.querySelector('.loader-wrapper')
 
