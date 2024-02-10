3cb3fcd7dbb074a1b53ac5bb8dc6a4b0f360456e
HP@LAPTOP <baxtibaybekniyazov@gmail.com>
2024-02-10 13:53:12 +0500

    Add js function which contact information integration  with telegram bot

--- js/script.js
+++ js/script.js
@@ -140,4 +140,49 @@ window.addEventListener('DOMContentLoaded', () => {
   const modalTimerId = setTimeout(openModal, 5000)
 
   // Modal ================------------================= end
+
+  // Form ================-------------================= start
+  const form = document.querySelector('form'),
+    telegramTokenBot = '6961519744:AAELRkGxSms-H0mvOqkXRjuSxN9i5f0_lW8',
+    chatId = '364298077'
+
+  const message = {
+    loading: 'Loading...',
+    success: 'Thanks for contacting with us',
+    failure: 'Something went wrong'
+  }
+
+  form.addEventListener('submit', (evt) => {
+    evt.preventDefault()
+
+    const statusMessage = document.createElement('div')
+    statusMessage.textContent = message.loading
+
+    form.append(statusMessage)
+    const formData = new FormData(form)
+
+    const object = {}
+    formData.forEach((value, key) => {
+      object[key] = value
+    })
+
+    fetch(`https://api.telegram.org/bot${telegramTokenBot}/sendMessage`, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        chat_id: chatId,
+        text: `
+          Name: ${object.name}, Phone: ${object.phone}
+        `
+      })
+    })
+      .then(() => (statusMessage.textContent = message.success))
+      .catch(() => statusMessage.failure)
+      .finally(() => {
+        setTimeout(() => {
+          statusMessage.remove()
+        }, 2000)
+      })
+  })
+  // Form ================-------------================= END
 })
